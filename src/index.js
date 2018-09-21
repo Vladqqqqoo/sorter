e21929ba3c45e00dc18305bdea88cd19fadd9e99
Vladislav Drobovich <vladdrobovich@gmail.com>
2018-09-21 23:05:38 +0300

    I'm still here

--- src/index.js
+++ src/index.js
@@ -1,30 +1,56 @@
 class Sorter {
   constructor() {
-    // your implementation
+		// your implementation
+		this.arr=[];
   }
 
   add(element) {
-    // your implementation
+		// your implementation
+		this.arr.push(element);
   }
 
   at(index) {
-    // your implementation
+		// your implementation
+		return this.arr[index];
   }
 
   get length() {
-    // your implementation
+		// your implementation
+		return this.arr.length;
   }
 
   toArray() {
-    // your implementation
+		// your implementation
+		return this.arr;
   }
 
   sort(indices) {
-    // your implementation
+		// your implementation
+		function compareNumeric(a, b) {
+			return a - b;
+		}
+
+		indices.sort(compareNumeric);
+
+		var tmpMass = [];
+
+		for(var i=0;i<indices.length;i++){
+			tmpMass[i]=this.arr[indices[i]];
+		}
+
+		if(this.compareFunction!=null)
+			tmpMass.sort(this.compareFunction)
+		else
+			tmpMass.sort(compareNumeric);
+
+		for(var i=0;i<indices.length;i++){
+			this.arr[indices[i]]=tmpMass[i];
+		}
   }
 
   setComparator(compareFunction) {
-    // your implementation
+		// your implementation
+		this.compareFunction=compareFunction;
   }
 }
 
